60513548a20404d9cd0c2c087d72eeacd872903e
Hugo Bove <52443771+huboneo@users.noreply.github.com>
2020-01-21 14:07:19 +0100

    Update array-has-items.ts

--- src/utils/array-has-items.ts
+++ src/utils/array-has-items.ts
@@ -1,5 +1,5 @@
 const EMPTY = 0;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function arrayHasItems(arr: any): arr is Array<any> {
+export default function arrayHasItems(arr: any): boolean {
     return Array.isArray(arr) && arr.length > EMPTY;
 }
